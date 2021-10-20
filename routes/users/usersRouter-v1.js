92bdc7945bafa51ce0fd1b744605b8164edbf8c7
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-19 20:02:42 -0400

    finishes version 1 of CRUD, but runs into callback hell

---
+++ routes/users/usersRouter-v1.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+const userController = require("../users/controller/userController")
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// get all users
+router.get("/get-all-users", function(req,res){
+  userController.getAllUsers(function(err, payload) {
+    if (err) {
+      res.status(500).json({message: "Error", error: err});
+    } else {
+      res.json({message: "success", data: payload})
+    }
+  });
+  
+});
+
+// creat a new user
+router.post('/create-user', function(req, res){
+  userController.createUser(req.body, function(err, payload){
+    if (err) {
+      res.status(500).json({ message: "Error", error: err });
+    } else {
+      res.json({ message: "success", data: payload });
+    }
+  });
+});
+
+// update a user
+router.put('/update-user-by-id/:id', function(req,res){
+  userController.updateUserById(req.params.id, req.body, function(err, updatedPayload){
+    if(err){
+      res.status(500).json({ message: "Error", error: err });
+    } else {
+      res.json({ message: "success", data: updatedPayload});
+    }
+  })
+});
+
+// delete a user
+router.delete("/delete-user-by-id/:id", function(req, res){
+  userController.deleteUserById(req.params.id, function(err, deletedPayload){
+    if(err){
+      res.status(500).json({ message: "Error", error: err });
+    } else {
+      res.json({ message: "success", data: deletedPayload})
+    }
+  })
+})
+
+module.exports = router;
